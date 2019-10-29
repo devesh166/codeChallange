ff75b65f19c1910f8ca510826f0f63c234126b21
Devesh Bhardwaj <jiodevesh@gmail.com>
2019-10-29 15:30:23 +0530

    Update Traingle.java

--- Traingle.java
+++ Traingle.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Triangle {
-	 public int maxSum(int triangle[][]) 
+	 public static int maxSum(int triangle[][]) 
 	    { int m = triangle[0].length;
 		  int n = triangle.length;
 	        for (int i = m - 1; i >= 0; i--) 
@@ -23,7 +23,10 @@ public class Triangle {
 	
 	        return triangle[0][0]; 
 	    } 
-	 public int TriangleInput() {
+//	 public int TriangleInput() {
+//		
+//	 }
+	public static void main(String[] args) {
 		 int totalRow = 100;
 			char[][] myArray = new char[totalRow][];
 			File file = new File("inputTriangle.txt");
@@ -33,20 +36,18 @@ public class Triangle {
 				for (int row = 0; scanner.hasNextLine() && row < totalRow; row++) {
 					myArray[row] = scanner.nextLine().toCharArray();
 				}
+				int arr[][] = new int[myArray.length][];
 				for(int j=0;j<myArray.length;j++) {
 					for(int k=0;k<myArray[j].length;k++) {
-						
+						arr[j][k] = (int)myArray[j][k];
 					}
 				}
-				return maxSum(myArray);
+				System.out.println( maxSum(arr));
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	 }
-//	public static void main(String[] args) {
-//		
-//
-//	}
+
+	}
 
 }
